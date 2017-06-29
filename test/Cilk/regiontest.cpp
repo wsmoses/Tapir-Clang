9ea3e02ca03c9cd94166b0a3dfd32cf97aa78b1f
TB Schardl <neboat@mit.edu>
2017-06-29 14:06:41 +0000

    [test/Cilk] Adding some placeholder Cilk tests.

---
+++ test/Cilk/regiontest.cpp
@@ -0,0 +1,12 @@
+int bar();
+int baz(int);
+
+int foo(int n) {
+  int x = _Cilk_spawn bar();
+  _Cilk_for(int i = 0; i < n; ++i) {
+    baz(i);
+  }
+  int y = _Cilk_spawn baz(n);
+  _Cilk_sync;
+  return x+y;
+}
