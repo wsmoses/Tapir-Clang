4a722288b688a70f9acd9265c9224f447e401e92
TB Schardl <neboat@mit.edu>
2017-07-10 13:08:58 +0000

    [CSI] Re-added the -fcsi flag, which was removed by mistake in a previous merge.

--- lib/Driver/ToolChains/Clang.cpp
+++ lib/Driver/ToolChains/Clang.cpp
@@ -3241,6 +3241,9 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   const SanitizerArgs &Sanitize = getToolChain().getSanitizerArgs();
   Sanitize.addArgs(getToolChain(), Args, CmdArgs, InputType);
 
+  if (Args.hasArg(options::OPT_fcsi))
+    Args.AddLastArg(CmdArgs, options::OPT_fcsi);
+
   const XRayArgs &XRay = getToolChain().getXRayArgs();
   XRay.addArgs(getToolChain(), Args, CmdArgs, InputType);
 
