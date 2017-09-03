a74c03783b70009d74a58b002db5233635fc7e15
William S. Moses <gh@wsmoses.com>
2017-09-03 14:32:50 -0400

    Fix failing tests

--- test/Cilk/looptest.cpp
+++ test/Cilk/looptest.cpp
@@ -1,20 +1,23 @@
 // RUN: %clang_cc1 -std=c++1z -verify %s
+// expected-no-diagnostics
+
+int null();
 
 int Cilk_for_tests(int n) {
   /* int n = 10; */
   /* _Cilk_for(int i = 0; i < n; i += 2); */
   /* _Cilk_for(int j = 0, __begin = 0, __end = n/2; __begin < __end; j += 2, __begin++); */
-  _Cilk_for (int i = 0; i < n; ++i);
-  _Cilk_for (int i = 0, __end = n; i < __end; ++i);
+  _Cilk_for (int i = 0; i < n; ++i) null();
+  _Cilk_for (int i = 0, __end = n; i < __end; ++i) null();
   unsigned long long m = 10;
-  _Cilk_for (int i = 0; i < m; ++i);
-  _Cilk_for (int i = 0, __end = m; i < __end; ++i);
+  _Cilk_for (int i = 0; i < m; ++i) null();
+  _Cilk_for (int i = 0, __end = m; i < __end; ++i) null();
   return 0;
 }
 
 int pragma_tests(int n) {
 #pragma clang loop unroll_count(4)
-  _Cilk_for (int i = 0; i < n; ++i);
+  _Cilk_for (int i = 0; i < n; ++i) null();
 
   return 0;
 }
--- test/Cilk/regiontest.cpp
+++ test/Cilk/regiontest.cpp
@@ -1,3 +1,6 @@
+// RUN: %clang_cc1 -std=c++1z -verify %s
+// expected-no-diagnostics
+
 int bar();
 int baz(int);
 
